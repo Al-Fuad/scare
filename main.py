f9c3842616b4f26614078d5a2e0e1fe6f159ce76
AlFuad <nur22205341056@diu.edu.bd>
2023-01-06 15:18:02 +0600

    Scary Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+from time import sleep
+pygame.init()
+window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+pygame.mixer.init()
+pygame.mixer.music.load('scary2.mp3')
+pygame.mixer.music.play()
+sleep(7)
+pygame.mixer.music.load('scary.mp3')
+pygame.mixer.music.play()
+sleep(1/2)
+image = pygame.image.load('scr.jpg')
+window.blit(image, (0,0))
+pygame.display.update()
+sleep(3)
